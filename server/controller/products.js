1752ac8ba52a5038227de09adf671a097c2a3c3d
robertobirijnr <robertobirijnr@hotmail.com>
2021-02-15 13:39:36 +0000

    get all products

--- server/controller/products.js
+++ server/controller/products.js
@@ -4,8 +4,29 @@ const Product = require('../module/product')
 const fs = require('fs')
 const { errorHandler } = require('../middlewares/errorHandler')
 
-exports.getAllProducts=(req,res)=>{
 
+//  get all products base on arrivals and sells
+//  by arrival = /products?sortBy=createdAt&order=desc
+
+exports.getAllProducts=(req,res)=>{
+    let order = req.query.order ? req.query.order :'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy :'_id'
+    let limit = req.query.limit ? parseInt(req.query.limit):6
+
+    Product.find()
+            .select('-photo')
+            .populate('category')
+            .sort([[sortBy,order]])
+            .limit(limit)
+            .exec((err,product)=>{
+                if(err){
+                    return res.status(400).json({
+                        error:"Products not found"
+                    })
+                }
+
+                res.send(product)
+            })
 }
 
 exports.createProduct =(req,res)=>{
@@ -141,3 +162,5 @@ exports.updateProduct =(req,res)=>{
         })
     })
 }
+
+
--- server/routes/products.js
+++ server/routes/products.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 
 router
-    .route('/product')
+    .route('/products')
     .get(getAllProducts)
 
 
